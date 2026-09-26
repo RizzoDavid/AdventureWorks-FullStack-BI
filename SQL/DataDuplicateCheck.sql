4e2528252ee985eb1ab56d6ccdf993943f8b193d
David Rizzo <david@drizzo.dev>
2026-09-26 10:55:17 -0400

    Added dimGeographyView

--- SQL/DataDuplicateCheck.sql
+++
@@ -1,7 +0,0 @@
-USE AdventureWorksDW2025
-SELECT SalesOrderNumber,
-	SalesOrderLineNumber, 
-	COUNT(*) AS OccurrenceCount
-FROM FactResellerSales
-GROUP BY SalesOrderNumber, SalesOrderLineNumber
-HAVING COUNT(*) > 1;
